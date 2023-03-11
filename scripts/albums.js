b16795287da655f49496eee5af65456ffc197425
kaoxiong20 <kaoxiong520@gmail.com>
2023-03-11 10:34:14 -0600

    Added true/false  boolean values to albums.js to indicate if an album is a favorite

--- scripts/albums.js
+++ scripts/albums.js
@@ -10,80 +10,70 @@ let albums = [
   { name: 'Heaven',
     artist: 'John Red',
     genre: 'Pop',
-    artwork: 'images/pexels-luis-quintero-1713953.jpg'
+    artwork: 'images/pexels-luis-quintero-1713953.jpg',
+    favorite: false;
   },
 
   { name: 'Relax',
     artist: 'Travis Blue',
     genre: 'Country',
-    artwork: 'images/pexels-inga-seliverstova-3394310.jpg'
+    artwork: 'images/pexels-inga-seliverstova-3394310.jpg',
+    favorite: false;
   },
 
   { name: 'Chill',
     artist: 'Logan Green',
     genre: 'Hip Hop',
-    artwork: 'images/pexels-lisa-fotios-2090881.jpg'
+    artwork: 'images/pexels-lisa-fotios-2090881.jpg',
+    favorite: true;
   },
 
   { name: 'Sunny Day',
     artist: 'Bryson Brown',
     genre: 'Rap',
-    artwork: 'images/pexels-miguel-á-padriñán-167092.jpg'
+    artwork: 'images/pexels-miguel-á-padriñán-167092.jpg',
+    favorite: false;
   },
 
   { name: 'Beach',
     artist: 'Da Toddler',
     genre: 'K-pop',
-    artwork: 'images/pexels-chedi-tanabene-1710795.jpg'
+    artwork: 'images/pexels-chedi-tanabene-1710795.jpg',
+    favorite: true;
   },
 
   { name: 'Vacay',
     artist: '3Pac',
     genre: 'Rap',
-    artwork: 'images/pexels-muffin-creatives-1646311.jpg'
+    artwork: 'images/pexels-muffin-creatives-1646311.jpg',
+    favorite: false;
   },
 
   { name: 'Lala Land',
     artist: 'Blue Star',
     genre: 'Pop',
-    artwork: 'images/pexels-olof-nyman-2170729.jpg'
+    artwork: 'images/pexels-olof-nyman-2170729.jpg',
+    favorite: false;
   },
 
   { name: 'Dream',
     artist: 'Purple Clouds',
     genre: 'Hip Hop',
-    artwork: 'images/pexels-dids-2742812.jpg'
+    artwork: 'images/pexels-dids-2742812.jpg',
+    favorite: true;
   },
 
   { name: 'Play',
     artist: 'Player',
     genre: 'Blues',
-    artwork: 'images/pexels-leah-kelley-3935703.jpg'
+    artwork: 'images/pexels-leah-kelley-3935703.jpg',
+    favorite: false;
   },
 
   { name: 'Happy',
     artist: 'Brown Rose',
     genre: 'Country',
-    artwork: 'images/pexels-mike-1181776.jpg'
+    artwork: 'images/pexels-mike-1181776.jpg',
+    favorite: false;
   }
 ];
-
-let favoriteAlbums = [
-  { name: 'Chill',
-    artist: 'Logan Green',
-    genre: 'Hip Hop',
-    artwork: 'images/pexels-lisa-fotios-2090881.jpg'
-  },
-
-{ name: 'Dream',
-  artist: 'Purple Clouds',
-  genre: 'Hip Hop',
-  artwork: 'images/pexels-dids-2742812.jpg'
-},
-
-{ name: 'Beach',
-artist: 'Da Toddler',
-genre: 'K-pop',
-artwork: 'images/pexels-chedi-tanabene-1710795.jpg'
-}
-]
